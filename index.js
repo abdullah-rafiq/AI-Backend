4760107ef4e86f25993fd6a69dc096386016bb7f
Abdullah <abdullah.joiya@outlook.com>
2025-12-15 07:06:31 +0500

    vision

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { InferenceClient, HfInference } = require('@huggingface/inference');
 const admin = require('firebase-admin');
 const fetch = require('node-fetch').default;
 require('dotenv').config();
-
 const app = express();
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use(cors());
 app.use(express.json());
 
